7bb42273e0ddce43ba453bafc400ccc1fd8bf4ee
Ganesh Dandavate <systechgd@gmail.com>
2025-02-13 07:54:51 +0530

    project setup finished

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div>
+      <h1>Hello</h1>
+    </div>
+  </StrictMode>,
+)
