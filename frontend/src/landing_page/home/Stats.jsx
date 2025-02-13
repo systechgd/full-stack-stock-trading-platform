8e76c0d5d034ede444cc465587af1246e76af07e
Ganesh Dandavate <systechgd@gmail.com>
2025-02-14 01:18:46 +0530

    Pricing and Education component finished

--- frontend/src/landing_page/home/Stats.jsx
+++ frontend/src/landing_page/home/Stats.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Stats() {
     return ( 
-        <div className="container p-3">
+        <div className="container p-2">
         <div className="row p-5">
           <div className="col-6 p-5">
             <h1 className="fs-2 mb-5">Trust with confidence</h1>
