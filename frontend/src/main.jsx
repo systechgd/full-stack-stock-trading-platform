d86eef8b26815670d7f04e19eef38e44fdf9cce3
Ganesh Dandavate <systechgd@gmail.com>
2025-02-14 00:50:56 +0530

    Home Hero and Award Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import HomePage from './landing_page/home/HomePage'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div>
-      <h1>Hello</h1>
-    </div>
+  <StrictMode> 
+      <HomePage/>
   </StrictMode>,
 )
