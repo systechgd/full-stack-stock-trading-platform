b1b83fafbf64b6fbb5a7710d0cf67a73190871d2
Ganesh Dandavate <systechgd@gmail.com>
2025-02-14 22:35:05 +0530

    modified universe section

--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 function Universe() {
-    return ( 
-        <div className="container mt-5">
+  return (
+    <div className="container mt-5">
       <div className="row text-center">
         <h1>The Zerodha Universe</h1>
         <p>
@@ -11,28 +11,41 @@ function Universe() {
         </p>
 
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/smallcaseLogo.png" style={{maxWidth: "100%"}}/>
+          <p className="text-small text-muted p-3">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/zerodhaFundhouse.png" style={{maxWidth: "50%"}} />
+          <p className="text-small text-muted p-3">Our asset management venture
+that is creating simple and transparent index
+funds to help you save for your goals.</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/sensibullLogo.svg" style={{width: "60%"}}/>
+          <p className="text-small text-muted p-3">Options trading platform that lets you
+create strategies, analyze positions, and examine
+data points like open interest, FII/DII, and more.</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/streakLogo.png" style={{maxWidth: "45%"}}/>
+          <p className="text-small text-muted p-3">Systematic trading platform
+that allows you to create and backtest
+strategies without coding.</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/smallcaseLogo.png" style={{maxWidth: "100%"}}/>
+          <p className="text-small text-muted p-3">TThematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media/images/dittoLogo.png" style={{maxWidth: "30%"}}/>
+          <p className="text-small text-muted p-3">Personalized advice on life
+and health insurance. No spam
+and no mis-selling.
+Sign up for free</p>
         </div>
         <button
           className="p-2 btn btn-primary fs-5 mb-5"
@@ -42,7 +55,7 @@ function Universe() {
         </button>
       </div>
     </div>
-     );
+  );
 }
 
 export default Universe;
