7dff1cc254f33610b1417c77704f3c1034c6b26c
Ganesh Dandavate <systechgd@gmail.com>
2025-02-14 22:11:35 +0530

    product component finished

--- frontend/src/landing_page/products/RightSection.jsx
+++ frontend/src/landing_page/products/RightSection.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
-function RightSection() {
+function RightSection({ imageURL, productName, productDesription, learnMore }) {
     return ( 
-        <h1>RightSection</h1>
+        <div className="container mt-5">
+        <div className="row">
+          <div className="col-6 p-5 mt-5">
+            <h1>{productName}</h1>
+            <p>{productDesription}</p>
+            <div>
+              <a href={learnMore}>Learn More</a>
+            </div>
+          </div>
+          <div className="col-6">
+            <img src={imageURL} />
+          </div>
+        </div>
+      </div>
      );
 }
 
