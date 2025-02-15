3f25621a115e7f4a2c0a18631067ab74d8e0aa8b
Ganesh Dandavate <systechgd@gmail.com>
2025-02-15 08:49:47 +0530

    dashboard created

---
+++ dashboard/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TopBar from './TopBar';
+import Dashboard from './Dashboard';
+
+const Apps = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default Apps;
