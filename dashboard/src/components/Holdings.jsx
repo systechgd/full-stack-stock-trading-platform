de1015f22c6b6606c385c7bf934e1e8fa8fc1de8
Ganesh Dandavate <systechgd@gmail.com>
2025-02-17 18:53:53 +0530

    fetching data from db to dashboard

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { holdings } from "../data/data";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+// import { holdings } from "../data/data";
 
 const Holdings = () => {
+
+  const [allHoldings, setallHoldings] = useState([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:3333/allHoldings").then((res) =>{
+      console.log(res.data);
+      setallHoldings(res.data);
+    })
+  }, []);
+
   return (
     <>
-      <h3 className="title">Holdings ({holdings.length})</h3>
+      <h3 className="title">Holdings ({allHoldings.length})</h3>
 
       <div className="order-table">
         <table>
@@ -18,7 +29,7 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
-          {holdings.map((stock, index) => {
+          {allHoldings.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
