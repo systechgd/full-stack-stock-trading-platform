36f02a2a8e23630c01913788c034a6cb016ada24
Ganesh Dandavate <systechgd@gmail.com>
2025-02-17 18:53:31 +0530

    functionality added that fetching data from db to dashboard

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,68 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const {HoldingsModel} = require('./model/HoldingsModel');
 const {PositionsModel} = require('./model/PositionsModel');
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 const uri = process.env.MONGO_URL;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 // To load temporary data to mongodb
-app.get('/addPositions', async(req, res) =>{
-  let tempPositions = [
-    {
-      product: "CNC",
-      name: "EVEREADY",
-      qty: 2,
-      avg: 316.27,
-      price: 312.35,
-      net: "+0.58%",
-      day: "-1.24%",
-      isLoss: true,
-    },
-    {
-      product: "CNC",
-      name: "JUBLFOOD",
-      qty: 1,
-      avg: 3124.75,
-      price: 3082.65,
-      net: "+10.04%",
-      day: "-1.35%",
-      isLoss: true,
-    },
-  ];
+// app.get('/addPositions', async(req, res) =>{
+//   let tempPositions = [
+//     {
+//       product: "CNC",
+//       name: "EVEREADY",
+//       qty: 2,
+//       avg: 316.27,
+//       price: 312.35,
+//       net: "+0.58%",
+//       day: "-1.24%",
+//       isLoss: true,
+//     },
+//     {
+//       product: "CNC",
+//       name: "JUBLFOOD",
+//       qty: 1,
+//       avg: 3124.75,
+//       price: 3082.65,
+//       net: "+10.04%",
+//       day: "-1.35%",
+//       isLoss: true,
+//     },
+//   ];
 
-  tempPositions.forEach((item) =>{
-    let newPositionsModel = new PositionsModel({
-      name: item.name,
-      qty: item.qty,
-      avg: item.avg,
-      price: item.price,
-      net: item.net,
-      day: item.day,
-    });
+//   tempPositions.forEach((item) =>{
+//     let newPositionsModel = new PositionsModel({
+//       name: item.name,
+//       qty: item.qty,
+//       avg: item.avg,
+//       price: item.price,
+//       net: item.net,
+//       day: item.day,
+//     });
 
-    newPositionsModel.save();
-  });
-  res.send("Done!");
+//     newPositionsModel.save();
+//   });
+//   res.send("Done!");
+// });
+
+app.get('/allHoldings', async(req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
 });
 
+app.get('/allPositions', async(req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+})
 
 
 
