53e1468f1c180d7315488f6d56ac1f8a57461862
Ganesh Dandavate <systechgd@gmail.com>
2025-02-20 18:38:09 +0530

    charts added

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import { VerticalGraph } from "./VerticleGraph";
 // import { holdings } from "../data/data";
 
 const Holdings = () => {
@@ -12,6 +13,39 @@ const Holdings = () => {
       setallHoldings(res.data);
     })
   }, []);
+  
+
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const labels = allHoldings.map((subArray) => subArray["name"]);
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Stock Price',
+      data: allHoldings.map((stock) => stock.price),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ]
+}
+
+  // export const data = {
+  //   labels,
+  //   datasets: [
+      // {
+      //   label: 'Dataset 1',
+      //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      // },
+  //     {
+  //       label: 'Dataset 2',
+  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+  //     },
+  //   ],
+  // };
+
 
   return (
     <>
@@ -71,6 +105,9 @@ const Holdings = () => {
           <p>P&L</p>
         </div>
       </div>
+
+      <VerticalGraph data={data}/>
+      {console.log(data)}
     </>
   );
 };
--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -6,8 +6,39 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import GeneralContext from "./GeneralContext";
+import { DoughnutChart } from "./DoughnutChart";
+
+const labels = watchlist.map((subArray) => subArray["name"]);
 
 const WatchList = () => {
+
+  const data = {
+    labels,
+    datasets: [
+      {
+          label: "Price",
+          data: watchlist.map((stock) => stock.price),
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 159, 64, 0.5)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <div className="watchlist-container">
       <div className="search-container">
@@ -26,6 +57,7 @@ const WatchList = () => {
           return <WatchListItem stock={stock} key={index} />;
         })}
       </ul>
+      <DoughnutChart data={data}/>
     </div>
   );
 };
